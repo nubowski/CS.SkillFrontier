1c9029b98289b2c670bcd72923f720e1c784b680
nubowski <nubowski@nnteam.ru>
2023-07-09 01:00:07 +0300

    feat: add simple damage components and system to the demo project

---
+++ TestMyGame/Components/DamageComponent.cs
@@ -0,0 +1,8 @@
+﻿using CoreLibs.Components;
+
+namespace TestMyGame.Components;
+
+public class DamageComponent : IComponent
+{
+    public int Damage { get; set; }
+}
---
+++ TestMyGame/Systems/DamageSystem.cs
@@ -0,0 +1,32 @@
+﻿using CoreLibs.Components;
+using CoreLibs.Systems;
+using TestMyGame.Components;
+
+namespace TestMyGame.Systems;
+
+public class DamageSystem : ISystem
+{
+    private readonly IComponentRegistry _componentRegistry;
+
+
+    public DamageSystem(IComponentRegistry componentRegistry)
+    {
+        _componentRegistry = componentRegistry;
+    }
+
+    public void Update(float deltaTime)
+    {
+        var entitiesWithDamage = _componentRegistry.GetEntitiesWithComponent<DamageComponent>();
+
+        foreach (var entity in entitiesWithDamage)
+        {
+            var damage = entity.GetComponent<DamageComponent>();
+            var health = entity.GetComponent<HealthComponent>();
+            if (health != null)
+            {
+                health.CurrentHealth -= damage.Damage;
+                Console.WriteLine($"Entity {entity.Id} took {damage.Damage} damage. Current health: {health.CurrentHealth}");
+            }
+        }
+    }
+}
