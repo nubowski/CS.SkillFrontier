6cda993b6c659bcdea71d72ab44d891e6392b55b
nubowski <nubowski@nnteam.ru>
2023-07-09 00:50:43 +0300

    feat: add simple SystemManager.cs

---
+++ CoreLibs/Systems/SystemManager.cs
@@ -0,0 +1,19 @@
+﻿namespace CoreLibs.Systems;
+
+public class SystemManager
+{
+    private readonly List<ISystem> _systems = new List<ISystem>();
+
+    public void AddSystem(ISystem system)
+    {
+        _systems.Add(system);
+    }
+
+    public void Update(float deltaTime)
+    {
+        foreach (var system in _systems)
+        {
+            system.Update(deltaTime);
+        }
+    }
+}
