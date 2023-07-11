1e33ac3d3f30027df265adb71a22578e7f1ff659
nubowski <nubowski@nnteam.ru>
2023-07-12 01:11:06 +0300

    feat: add HasEvent method and KeyPress event

---
+++ CoreLibs/Events/EventList/KeypressEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreLibs.Events.EventList;
+
+public class KeypressEvent : IEvent
+{
+    public ConsoleKey Key { get; private set; }
+
+    public KeypressEvent(ConsoleKey key)
+    {
+        Key = key;
+    }
+}
--- CoreLibs/Events/EventManager.cs
+++ CoreLibs/Events/EventManager.cs
@@ -3,6 +3,7 @@
 public class EventManager
 {
     private Dictionary<Type, List<Action<IEvent>>> _listeners = new Dictionary<Type, List<Action<IEvent>>>();
+    private HashSet<Type> _emittedEvents = new HashSet<Type>(); // track of emitted events
 
     public void AddListener<T>(Action<T> listener) where T : IEvent
     {
@@ -11,7 +12,7 @@ public class EventManager
         {
             _listeners[eventType] = new List<Action<IEvent>>();
         }
-        _listeners[eventType].Add((e) => listener((T) e));
+        _listeners[eventType].Add((e) => listener((T)e));
     }
 
     public void RemoveListener<T>(Action<T> listener) where T : IEvent
@@ -19,7 +20,7 @@ public class EventManager
         var eventType = typeof(T);
         if (!_listeners.ContainsKey(eventType)) return;
 
-        _listeners[eventType].Remove((e) => listener((T) e));
+        _listeners[eventType].Remove((e) => listener((T)e));
 
         if (_listeners[eventType].Count == 0)
         {
@@ -30,11 +31,19 @@ public class EventManager
     public void Emit<T>(T emittedEvent) where T : IEvent
     {
         var eventType = typeof(T);
-        if(!_listeners.ContainsKey(eventType)) return;
+        if (!_listeners.ContainsKey(eventType)) return;
 
         foreach (var listener in _listeners[eventType])
         {
             listener(emittedEvent);
         }
+
+        _emittedEvents.Add(eventType); // event type as emitted
+    }
+
+    public bool HasEvent<T>() where T : IEvent // event type has been emitted
+    {
+        var eventType = typeof(T);
+        return _emittedEvents.Contains(eventType);
     }
 }
