7916ce9c75764a3addd2beadd16769b95f60c751
nubowski <nubowski@nnteam.ru>
2023-07-12 22:39:17 +0300

    feat: add DUMMY CombatSystem.cs
    // now is showing twice dice rolled enemy with primitive stats

--- TestMyGame/Systems/CombatSystem.cs
+++ TestMyGame/Systems/CombatSystem.cs
@@ -2,6 +2,7 @@
 using CoreLibs.Events;
 using CoreLibs.Systems;
 using TestMyGame.Components;
+using TestMyGame.Events;
 
 namespace TestMyGame.Systems;
 
@@ -11,14 +12,32 @@ public class CombatSystem : BaseSystem
     {
         _entityManager = entityManager;
         
+        _eventManager.AddListener<NpcCreatedEvent>(HandleStartFight);
+        
         ComponentFilter.MustHaveComponents.AddRange(new Type[]
         {
             typeof(CharacterComponent),
-            typeof(HealthComponent),
-            typeof(AttackComponent)
         });
     }
-    
+
+    private void HandleStartFight(NpcCreatedEvent npcCreatedEvent)
+    {
+        Entity npc = npcCreatedEvent.Npc;
+        
+        var name = npc.GetComponent<NameComponent>().Name;
+        var race = npc.GetComponent<RaceComponent>().Race;
+        var gender = npc.GetComponent<GenderComponent>().Gender;
+        var description = npc.GetComponent<DescriptionComponent>().Description;
+        
+        
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Race: {race}");
+        Console.WriteLine($"Gender: {gender}");
+        Console.WriteLine($"Description: {description}");
+        
+    }
+
     public override void Update(float deltaTime)
     {
         // combat goes here
