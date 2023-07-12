f5dc4ed771726b055a347e3ef2987c40d6964135
nubowski <nubowski@nnteam.ru>
2023-07-12 22:38:31 +0300

    fix: clean up systems

--- TestMyGame/Systems/IdleCombat/GrindingSystem.cs
+++ TestMyGame/Systems/IdleCombat/GrindingSystem.cs
@@ -23,7 +23,14 @@ public class GrindingSystem : BaseSystem
 
     private void HandleStartGrinding(StartIdleGrindingEvent startIdleGrindingEvent)
     {
-        _eventManager.Emit(new CreateNpcEvent());
+        // TODO delete these --v
+        // we need to take the location entity to check its name?
+        var locationId = startIdleGrindingEvent.Player.GetComponent<PositionComponent>().CurrentLocationId;
+        var locName = _entityManager.GetEntityById(locationId).GetComponent<LocationComponent>().Name;
+        var locDesc = _entityManager.GetEntityById(locationId).GetComponent<LocationComponent>().Description;
+
+        var player = startIdleGrindingEvent.Player;
+        _eventManager.Emit(new CreateNpcEvent(player));
 
         // Using this Id of the location to build and bring the spawn query and some advanced logic
     }
