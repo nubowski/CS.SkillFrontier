e4faf57abc37873f4cc2bc8072a5485c0fdf4f01
nubowski <nubowski@nnteam.ru>
2023-07-12 22:37:48 +0300

    fix: change an update() method
    // TODO realize that this is a good aproach (dont think so tho)

--- TestMyGame/Systems/LocationActionProcessingSystem.cs
+++ TestMyGame/Systems/LocationActionProcessingSystem.cs
@@ -1,7 +1,7 @@
 ﻿using CoreLibs.Entities;
 using CoreLibs.Events;
-using CoreLibs.Events.EventList;
 using CoreLibs.Systems;
+using CoreLibs.Utilities;
 using TestMyGame.Components;
 using TestMyGame.Events;
 
@@ -18,6 +18,18 @@ public class LocationActionProcessingSystem : BaseSystem
             typeof(LocationActionComponent)
         });
     }
+    
+    public override void Update(float deltaTime)
+    {
+        // Get all entities based on the component filter
+        var entities = _entityManager.GetEntitiesBasedOnFilter(ComponentFilter);
+
+        // Process each entity
+        foreach (var entity in entities)
+        {
+            ProcessEntity(entity, deltaTime);
+        }
+    }
 
     public override void ProcessEntity(Entity entity, float deltaTime)
     {
@@ -27,12 +39,15 @@ public class LocationActionProcessingSystem : BaseSystem
         {
             case LocationActionComponent.LocationAction.Grind:
                 _eventManager.Emit(new StartIdleGrindingEvent(entity));
+                Logger.Debug($"Emitted StartIdleGrindingEvent");
                 break;
             case LocationActionComponent.LocationAction.Stop:
                 _eventManager.Emit(new StopGrindingEvent(entity));
+                Logger.Debug($"Emitted StopGrindingEvent");
                 break;
             case LocationActionComponent.LocationAction.Explore:
                 _eventManager.Emit(new StartExploringEvent(entity));
+                Logger.Debug($"Emitted StartExploringEvent");
                 break;
         }
 
