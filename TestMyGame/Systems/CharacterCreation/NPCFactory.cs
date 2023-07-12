541b14ebc0bdd7aa7659ebb450907ac34afe4887
nubowski <nubowski@nnteam.ru>
2023-07-12 18:05:41 +0300

    feat: add NPC factory

---
+++ TestMyGame/Systems/CharacterCreation/NPCFactory.cs
@@ -0,0 +1,45 @@
+﻿using CoreLibs.Entities;
+using TestMyGame.Components;
+using TestMyGame.Components.Stimul;
+
+namespace TestMyGame.Systems.CharacterCreation;
+
+public class NPCFactory
+{
+    private readonly EntityManager _entityManager;
+    private readonly ComponentRegistry _componentRegistry;
+
+    public NPCFactory(EntityManager entityManager, ComponentRegistry componentRegistry)
+    {
+        _entityManager = entityManager;
+        _componentRegistry = componentRegistry;
+    }
+
+    public Entity CreateNPC(string npcType)
+    {
+        var entity = _entityManager.CreateEntity();
+
+        // STIMUL attributes
+        entity.AddComponent(new StrengthAttributeComponent());
+        entity.AddComponent(new ToughnessAttributeComponent());
+        entity.AddComponent(new IntelligenceAttributeComponent());
+        entity.AddComponent(new MindAttributeComponent());
+        entity.AddComponent(new LuckAttributeComponent());
+        entity.AddComponent(new AgilityAttributeComponent());
+
+        // Common components
+        entity.AddComponent(new CharacterComponent());
+        entity.AddComponent(new HealthComponent());
+        entity.AddComponent(new LevelComponent());
+        entity.AddComponent(new RaceComponent());
+        // entity.AddComponent(new CharacterTypeComponent);
+
+        // NPC-specific components
+        entity.AddComponent(new NPCTypeComponent());
+        entity.AddComponent(new PositionComponent());
+
+        // TODO: Add any other NPC-specific components
+
+        return entity;
+    }
+}
