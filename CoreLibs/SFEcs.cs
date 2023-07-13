f9daa97e8f7bd77bef0fd66577491d0e8e4b6b95
nubowski <nubowski@nnteam.ru>
2023-07-13 20:27:14 +0300

    feat: base Interfaces and 2 methods OnAwaker from Init and OnUpdate as ISystem

---
+++ CoreLibs/SFEcs.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreLibs;
+
+public interface IComponent
+{
+    
+}
+
+public interface IInitializer : IDisposable
+{
+    World World { get; set; }
+    
+    // system first call before update
+    void OnAwake();
+}
+
+public interface ISystem : IInitializer
+{
+    // system on each update cycle
+    void OnUpdate();
+}
