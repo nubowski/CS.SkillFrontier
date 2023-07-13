a258f543cddf65158fada64e6cafe2f989df2110
nubowski <nubowski@nnteam.ru>
2023-07-13 20:26:45 +0300

    feat: add EntityId.cs struct
    // need to add some overrides there?

---
+++ CoreLibs/EntityId.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreLibs;
+
+public struct EntityId
+{
+    public int Id { get; }
+    public int Gen { get; }
+
+    public EntityId(int id, int gen)
+    {
+        Id = id;
+        Gen = gen;
+    }
+
+    // TODO some equals and other override methods... here?
+}
